a7080c0cc10d9c99b071a6b7518ac9eee7ae8a68
Pav Singh <pav2013@yahoo.com>
2015-02-18 22:47:58 -0500

    completed gaint and terrestrailPlanet class.

--- src/GiantPlanet.java
+++ src/GiantPlanet.java
@@ -1,11 +1,37 @@
 /**
- * 
- */
-
-/**
  * @author PavSingh
- *
+ * @description Giant Planet object class which extends from the Planet class 
+ * and used the IHasRings and IHasMoons mathods.
+ * @date: 18 February 2015.
+ * @version: AbstractPlanet 1.0
  */
-public class GiantPlanet {
+	
+public class GiantPlanet extends Planet implements IHasMoons, IHasRings {
+    	// Private variables 
+	    private String _type;
+		
+        // Public Properities
+	    public String getType() {
+			return _type;
+		}
+		
+		// constructor
+		public GiantPlanet(String name, double diameter, double mass, String type){
+			super(name, diameter, mass);		
+			this._type = type;
+		}
+		
+		// overriden mathods
+		@Override
+		public Boolean HasRings() {
+			
+			return (this.getRingCount() > 0) ? true : false;
+		}
 
-}
+		@Override
+		public Boolean HasMoons() {
+			
+			return (this.getMoonCount() > 0) ? true : false;
+		}
+
+	}
--- src/TerrestrialPlanet.java
+++ src/TerrestrialPlanet.java
@@ -1,11 +1,33 @@
 /**
- * 
- */
-
-/**
  * @author PavSingh
- *
+ * @description This class is a	subclass of	the	Planet Abstract	class. The class will also	
+ * implement both the IHasMoons and	IHabitable interfaces.
+ * @date: 18 February 2015.
+ * @version: AbstractPlanet 1.0
  */
-public class TerrestrialPlanet {
+	
+public class TerrestrialPlanet extends Planet implements IHasMoons, IHabitable {
+
+	// private Variables 
+	private boolean _oxygen;
+	
+	// constructor
+	public TerrestrialPlanet(String name, double diameter, double mass, boolean oxygen){
+		super(name, diameter, mass);
+		this._oxygen = oxygen;
+		
+	}
+	
+	// overridden Methods
+	@Override
+	public Boolean Habitable() {
+		return (this._oxygen == true) ? true : false;
+	}
+
+	@Override
+	public Boolean HasMoons() {		
+		return (this.getMoonCount() > 0) ? true : false;
+		
+	}
 
 }
