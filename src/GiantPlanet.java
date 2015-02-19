2ed6f8a481561a1997be7740a3034eb911d64a0f
Pav Singh <pav2013@yahoo.com>
2015-02-18 21:37:24 -0500

    Start with planet class

---
+++ src/GiantPlanet.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+/**
+ * @author PavSingh
+ *
+ */
+public class GiantPlanet {
+
+}
---
+++ src/TerrestrialPlanet.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+/**
+ * @author PavSingh
+ *
+ */
+public class TerrestrialPlanet {
+
+}
